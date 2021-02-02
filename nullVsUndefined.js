2dc30df9024a5dfdad8d093a26711c4bf9271460
Ashraf-7492 <aukcs89@gmail.com>
2021-02-03 01:07:05 +0600

    nullvsundefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,12 @@
+// let pakhi;
+// if function is not returned it will show undefined
+// function add (num1, num2){
+//     console.log(num1 + num2);
+//     return
+
+// }
+function add (num1, num2){
+    console.log(num1, num2);
+}
+const result = add(25);
+console.log(result);
